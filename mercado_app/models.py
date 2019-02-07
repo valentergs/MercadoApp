fdebd1a243b2896cc956cd5d95cf382aa97aeaff
Rodrigo Valente <rodrigovalente@MacBook-Pro-de-Rodrigo.local>
2019-02-07 13:44:37 -0200

    models.py migration

--- mercado_app/models.py
+++ mercado_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=150)
+    descrição = models.CharField(max_length=200, blank=True)
+    seção = models.CharField(max_length=120, blank=True)
+
+
+class Loja(models.Model):
+    nome = models.CharField(max_length=150)
+    local = models.CharField(max_length=150)
+
+
+class Lista(models.Model):
+    criada_em = models.DateTimeField(auto_now_add=True)
+    loja = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Loja, on_delete=models.CASCADE)
+    preço = models.IntegerField()
--- mercado_proj/settings.py
+++ mercado_proj/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'knox',
+    'mercado_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
